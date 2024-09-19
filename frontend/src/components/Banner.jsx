4575bb3db3e3b1558b86ce33b6f444a74125772f
llam280 <llam280@aucklanduni.ac.nz>
2024-09-19 21:00:55 +1200

    added sign out button

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -4,6 +4,12 @@ const Banner = () => {
     return (
         <div className="bg-primary flex flex-row w-[100%] py-[1%] px-[2%]">
             <img src={FintrackLogo} alt="logo" className="w-[15%]" />
+
+            <button
+                className="absolute top-50 right-10 bg-primary-red text-white text-button px-3 py-3 min-w-[200px] rounded-full hover:bg-primary-red-dark active:bg-primary-red-darker"
+            >
+                Sign Out
+            </button>
         </div>
     );
 }
