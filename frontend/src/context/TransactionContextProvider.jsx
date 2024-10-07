e1e37f214f70df1be9ea9ccda3a9d764570e3e92
llam280 <llam280@aucklanduni.ac.nz>
2024-10-08 12:29:04 +1300

    removed unneccessary reactive values

--- frontend/src/context/TransactionContextProvider.jsx
+++ frontend/src/context/TransactionContextProvider.jsx
@@ -33,7 +33,7 @@ export function TransactionContextProvider({ children }) {
         window.location.href = "/login";
         console.error("Not logged in ", error);
       });
-  }, [currency, balance, transactions, uiUpdateRequest]);
+  }, [transactions]);
 
   // Fetch the user's current savings goal and convert to specified currency when the component mounts
   useEffect(() => { refreshDisplayGoal(setGoal, currency); }, [goal, currency]);
@@ -64,7 +64,7 @@ export function TransactionContextProvider({ children }) {
         window.location.href = "/login";
         console.error("Not logged in ", error);
       });
-  }, [currentPage, filter, balance, uiUpdateRequest]);
+  }, [currentPage, filter]);
 
   // function to return the transactions for a given page. Return empty array if a page has no transactions
   const returnTransactionsPerPage = (transactions, currentPage, pageSize) => {
